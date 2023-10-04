33fc0f21b9c8aacf2703832b2cdedf33448ccada
sosiska52 <nikuta.makarevich@gmail.com>
2023-10-05 00:06:15 +0300

    ppois_5

---
+++ 2 year/PPOIS/src/ppois_5/ppois_5/ppois_5.cpp
@@ -0,0 +1,90 @@
+﻿#include <iostream>
+
+using namespace std;
+
+class Weapon {
+protected:
+	string name;
+	int maxCapacity;
+	bool* magazine;
+	int bulletsLeft;
+	int ind;
+public:
+	int showRemainingBullets() { return bulletsLeft; }
+
+	string showName() { return name; }
+
+	void reload() {
+		for (int i = 0; i < ind; i++) 
+			magazine[i] = true;
+		bulletsLeft = maxCapacity;
+		ind = 0;
+		cout << "reloaded" << endl;
+	}
+
+	virtual void fire() {
+		cout << "ERROR" << endl;
+	}
+};
+
+class MachineGun : public Weapon {
+public:
+	MachineGun(string name) {
+		this->name = name;
+		maxCapacity = 150;
+		bulletsLeft = 0;
+		ind = 0;
+		magazine = new bool[maxCapacity];
+	}
+
+	void fire() {
+		if(bulletsLeft >= 5){
+			cout << "RATATATATA" << endl;
+			bulletsLeft -= 5;
+			ind += 5;
+			for (int i = 0; i < ind; i++)
+				magazine[i] = false;
+		}
+		else {
+			cout << "NO AMMO" << endl;
+		}
+	}
+};
+
+class Revolver : public Weapon {
+public:
+	Revolver(string name) {
+		this->name = name;
+		maxCapacity = 6;
+		bulletsLeft = 0;
+		ind = 0;
+		magazine = new bool[maxCapacity];
+	}
+
+	void fire() {
+		if (bulletsLeft >= 1) {
+			cout << "POW" << endl;
+			bulletsLeft--;
+			magazine[ind] = false;
+			ind ++;
+		}
+		else {
+			cout << "NO AMMO" << endl;
+		}
+	}
+};
+
+int main() {
+	Weapon* weapon = new Weapon;
+	weapon->fire();
+	MachineGun machineGun("Gatling");
+	weapon = &machineGun;
+	weapon->reload();
+	weapon->fire();
+	Revolver rev("Magnum");
+	rev.reload();
+	while(rev.showRemainingBullets() > 0)
+		rev.fire();
+	rev.fire();
+	return 0;
+}
