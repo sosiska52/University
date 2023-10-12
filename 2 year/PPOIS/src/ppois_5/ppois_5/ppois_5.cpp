2a3834c06cbf7795dc915cd61f77266157f8a457
sosiska52 <nikuta.makarevich@gmail.com>
2023-10-12 11:24:21 +0300

    ppois fix

--- 2 year/PPOIS/src/ppois_5/ppois_5/ppois_5.cpp
+++ 2 year/PPOIS/src/ppois_5/ppois_5/ppois_5.cpp
@@ -3,14 +3,26 @@
 using namespace std;
 
 class MachineGun {
+private:
+	int mag;
 public:
+	MachineGun(int temp) {
+		this->mag = temp;
+	}
+
 	void fire() {
 		cout << "RATATATATA" << endl;
 	}
 };
 
 class Revolver {
+private:
+	string name;
 public:
+	Revolver(string name) {
+		this->name = name;
+	}
+
 	void fire() {
 		cout << "POW" << endl;
 	}
@@ -20,6 +32,9 @@ class Glock : public MachineGun, public Revolver {
 private:
 	bool mode;
 public:
+	Glock(int temp, string name) : MachineGun(temp), Revolver(name) {
+		mode = false;
+	}
 	void changeMode() {
 		mode = !mode;
 	}
@@ -32,7 +47,7 @@ public:
 };
 
 int main() {
-	Glock glock;
+	Glock glock(12, "Pistol");
 	glock.fire();
 	glock.changeMode();
 	glock.fire();
