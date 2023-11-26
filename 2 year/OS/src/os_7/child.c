edbaa065c50d56412c34cca52c9985d0dc06c38b
sosiska52 <nikuta.makarevich@gmail.com>
2023-11-27 01:23:01 +0300

    os_7 fix

--- 2 year/OS/src/os_7/child.c
+++ 2 year/OS/src/os_7/child.c
@@ -1,71 +1,46 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <semaphore.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <fcntl.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <string.h>
+#include <stdio.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <unistd.h>
+#define buf 1
+int main()
+{
+char symbol;
+int value;
+int sum = 0;
 
-int main() {
+printf("Consumer\n");
+sem_t *full;
+sem_t *empty;
+sem_t *temp;
+const char *sem_full="full";
+const char *sem_empty="empty";
+const char *sem_temp = "temp";
 
-key_t shm_key;
-    int shm_id;
-    char *shm_ptr;
-    const char *shm_name = "/shared_memory";
+full= sem_open(sem_full,O_CREAT,0777,0);
+empty= sem_open(sem_empty,O_CREAT,0777,buf);
+temp = sem_open(sem_temp, O_CREAT, 0777, 0);
 
-    // Генерируем уникальный ключ для сегмента разделяемой памяти
-    shm_key = ftok(".", 'R');
-    if (shm_key == -1) {
-        perror("ftok");
-        exit(1);
-    }
+while(1){
+sem_wait(full);
+int num;
+sem_getvalue(temp, &num);
+value = num - sum;
+sum += value;
+symbol = (char)value;
 
-    // Получаем сегмент разделяемой памяти, созданный отправителем
-    shm_id = shmget(shm_key, sizeof(char), 0666);
-    if (shm_id == -1) {
-        perror("shmget");
-        exit(1);
-    }
+printf("Got symbol: %c \n", symbol);
+sem_post(empty);
+}
 
-    // Присоединяем сегмент разделяемой памяти
-    shm_ptr = (char *)shmat(shm_id, NULL, 0);
-    if (shm_ptr == (void*) -1) {
-        perror("shmat");
-        exit(1);
-    }
-
-    sem_t *sem1 = sem_open("/semaphore1", O_CREAT, 0644, 1); // Создание семафора
-    sem_t *sem2 = sem_open("/semaphore2", O_CREAT, 0644, 0); // Создание семафора
-
-    if (sem1 == SEM_FAILED || sem2 == SEM_FAILED) {
-        perror("Ошибка создания семафора");
-        return 1;
-    }
-
-
-    while (1) {
-        sem_wait(sem2); // Ожидание семафора
-	char symbol = *shm_ptr;
-        printf("Процесс 2 получил символ: %c. \n", symbol);
-
-        int numPrints = rand() % 10 + 1; // Генерация случайного количества повторений
-
-        for (int i = 0; i < numPrints; i++) {
-            printf("Символ %c выводится на экран (раз %d/%d)\n", symbol, i + 1, numPrints);
-            usleep(50000); // Задержка в микросекундах
-        }
-
-        sem_post(sem1); // Установка семафора
-    }
-// Отсоединяем сегмент разделяемой памяти
-    if (shmdt(shm_ptr) == -1) {
-        perror("shmdt");
-        exit(1);
-    }
-    sem_close(sem1); // Закрытие семафора
-    sem_close(sem2); // Закрытие семафора
-
-    return 0;
+sem_close(full);
+sem_close(empty);
+sem_unlink(sem_full);
+sem_unlink(sem_empty);
+return 0;
 }
 
--- 2 year/OS/src/os_7/parent.c
+++ 2 year/OS/src/os_7/parent.c
@@ -1,76 +1,44 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <unistd.h>
 #include <semaphore.h>
+#include <sys/types.h>
+#include <unistd.h>
 #include <fcntl.h>
-#include <sys/ipc.h>
-#include <sys/shm.h>
-#include <string.h>
+#include <stdio.h>
+#include <sys/msg.h>
+#include <sys/sem.h>
+#include <unistd.h>
+#define buf 1
+int main()
+{
+char symbol;
+int value;
 
+printf("Producer\n");
+sem_t *full;
+sem_t *empty;
+sem_t *temp;
+const char *sem_full="full";
+const char *sem_empty="empty";
+const char *sem_temp = "temp";
 
-int main() {
-    key_t shm_key;
-    int shm_id;
-    char *shm_ptr;
-    const char *shm_name = "/shared_memory";
+full= sem_open(sem_full,O_CREAT,0777,0);
+empty= sem_open(sem_empty,O_CREAT,0777,buf);
+temp = sem_open(sem_temp, O_CREAT, 0777, 0);
 
-    // Генерируем уникальный ключ для сегмента разделяемой памяти
-    shm_key = ftok(".", 'R');
-    if (shm_key == -1) {
-        perror("ftok");
-        exit(1);
-    }
+while(1){
+sem_wait(empty);
+printf("Enter symbol: ");
+scanf(" %s", &symbol);
+value = (int)symbol;
 
-    // Создаем сегмент разделяемой памяти
-    shm_id = shmget(shm_key, sizeof(char), IPC_CREAT | 0666);
-    if (shm_id == -1) {
-        perror("shmget");
-        exit(1);
-    }
-
-    // Присоединяем сегмент разделяемой памяти
-    shm_ptr = (char *)shmat(shm_id, NULL, 0);
-    if (shm_ptr == (void*) -1) {
-        perror("shmat");
-        exit(1);
-    }
-
-    sem_t *sem1 = sem_open("/semaphore1", O_CREAT, 0644, 1); // Создание семафора
-    sem_t *sem2 = sem_open("/semaphore2", O_CREAT, 0644, 0); // Создание семафора
-
-    if (sem1 == SEM_FAILED || sem2 == SEM_FAILED) {
-        perror("Ошибка создания семафора");
-        return 1;
-    }
-
-    char symbol;
-
-    while (1) {
-        printf("Введите символ: ");
-        scanf(" %c", &symbol);
-        
-        *shm_ptr = symbol;
-
-        sem_wait(sem1); // Ожидание семафора
-
-        printf("Процесс 1 передал символ: %c\n", symbol);
-
-        sem_post(sem2); // Установка семафора
-    }
-	// Отсоединяем сегмент разделяемой памяти
-    if (shmdt(shm_ptr) == -1) {
-        perror("shmdt");
-        exit(1);
-    }
-
-    // Удаляем сегмент разделяемой памяти
-    if (shmctl(shm_id, IPC_RMID, NULL) == -1) {
-        perror("shmctl");
-        exit(1);
-    }
-    sem_close(sem1); // Закрытие семафора
-    sem_close(sem2); // Закрытие семафора
-
-    return 0;
+for(int i = 0; i < value; i++){
+	sem_post(temp);
+}
+sem_post(full);
 }
 
+sem_close(full);
+sem_close(mutex);
+sem_unlink(sem_full);
+sem_unlink(sem_empty);
+return 0;
+}
