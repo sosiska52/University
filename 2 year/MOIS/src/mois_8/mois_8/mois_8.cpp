ef80b287b23a41379670c495fd724417a28d0ff9
sosiska52 <nikuta.makarevich@gmail.com>
2024-02-19 00:24:45 +0300

    mois_8 menu

---
+++ 2 year/MOIS/src/mois_8/mois_8/mois_8.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <fstream>
+#include <vector>
+
+void makeMagaradga(int length) {
+	std::vector<std::vector<int>> matrix(length, std::vector<int>(length, 0));
+
+
+}
+
+void menu() {
+	int choise;
+	int length = NULL;
+	while (true) {
+		std::cout << "Menu\n";
+		std::cout << "1)Make matrix\n";
+		std::cout << "2)Exit\n";
+		std::cin >> choise;
+		switch (choise)
+		{
+		case 1:
+			system("cls");
+			std::cout << "Enter length: ";
+			std::cin >> length;
+
+		default:
+			system("cls");
+			std::cout << "Wrong choise\n";
+			system("pause");
+			system("cls");
+			break;
+		}
+	}
+}
+
+int main()
+{
+	menu();
+	return 0;
+}
