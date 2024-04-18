498f18e744ab0fc47ccbe311502e6d68179b7903
sosiska52 <nikuta.makarevich@gmail.com>
2024-04-18 09:28:40 +0300

    kmzi_4

--- 2 year/KMZI/src/kmzi_3/kmzi_3/kmzi_3.cpp
+++ 2 year/KMZI/src/kmzi_3/kmzi_3/kmzi_3.cpp
@@ -3,7 +3,7 @@
 
 int main()
 {
-	RandomGenerator rand(3, 5, 11);
+	RandomGenerator rand(3, 5, 64);
 	std::vector<int> res = rand.generate(1,100);
 	for (int i = 0; i < res.size(); i++)
 		std::cout << res[i] << " ";
---
+++ 2 year/KMZI/src/kmzi_4/kmzi_4/kmzi_4.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+#include <cmath>
+
+bool checkIfSimple(int n) {
+    double dn = (double)n;
+    double fractPart;
+    fractPart = std::modf(std::sqrt(dn), &fractPart);
+    if( == 0)
+    int r = 2;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
