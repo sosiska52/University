fd1bccd1a6438e7d4da865e625514b092c76d3cb
sosiska52 <nikuta.makarevich@gmail.com>
2024-05-19 01:02:53 +0300

    kmzi

--- 2 year/KMZI/src/kmzi_4/kmzi_4/kmzi_4.cpp
+++ 2 year/KMZI/src/kmzi_4/kmzi_4/kmzi_4.cpp
@@ -1,15 +1,114 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <vector>
 #include <cmath>
+#include <conio.h>
 
-bool checkIfSimple(int n) {
-    double dn = (double)n;
-    double fractPart;
-    fractPart = std::modf(std::sqrt(dn), &fractPart);
-    if( == 0)
-    int r = 2;
+void readrimeNumbers(std::vector<int>& vec) {
+    std::ifstream fin("prime_numbers.txt");
+    vec.resize(300);
+    for (int i = 0; i < 300; i++) {
+        fin >> vec[i];
+    }
+    fin.close();
+}
+
+int findGCD(int a, int b) {
+    while (b != 0) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+bool isPrime(int num) {
+    if (num < 2) {
+        return false;
+    }
+
+    for (int i = 2; i * i <= num; ++i) {
+        if (num % i == 0) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
+int findLargestPrimeDivisor(int num) {
+    int largestPrime = 2;
+
+    if (isPrime(num)) {
+        return num;
+    }
+
+    for (int i = 2; i <= num / 2; ++i) 
+        if (num % i == 0 && isPrime(i))
+            largestPrime = i;
+
+    return largestPrime;
+}
+
+bool checkIfPrime(int n) {
+    if (n < 2)
+        return false;
+
+    std::vector<int> primeNumbers;
+    readrimeNumbers(primeNumbers);
+    for (int i = 0; i < 300; i++) {
+        if (n == primeNumbers[i])
+            return true;
+        if (n % primeNumbers[i] == 0)
+            return false;
+    }
+       
+    int r = 1;
+    while (r < n) {
+        if (findGCD(r, n) != 1)
+            return false;
+        if (isPrime(r)) {
+            int q = findLargestPrimeDivisor(r - 1);
+            if (q > 4 * sqrt(r)*log2(n) && (int)pow(n, (r - 1) / q) % r == 1)
+                break;
+        }
+        r++;
+    }
+    
+    if (r == n)
+        return false;
+    
+    if (n - 1 <= 2 * sqrt(r) * log2(n)) {
+        for (int a = r; a < n; a++) 
+            if (findGCD(a, n) != 1)
+                return false;
+    }
+    else
+        for (int a = 1; a <= 2 * sqrt(r) * log2(n); a++)
+            if ((int)pow((3 - a), n) % ((int)pow(3, r) - 1) != (int)pow(3, n) - a)
+                return false;
+    
+    return true;
+}
+
+void menu() {
+    int number;
+    while(true) {
+        system("cls");
+        std::cout << "Enter number (0 to exit): ";
+        std::cin >> number;
+        if (number == 0)
+            break;
+        if (isPrime(number))
+            std::cout << "Prime\n(Press Enter)\n";
+        else
+            std::cout << "Complex\n(Press Enter)\n";
+        _getch();
+    }
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    menu();
+    return 0;
 }
---
+++ 2 year/KMZI/src/kmzi_5/kmzi_5/kmzi_5.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <cmath>
+
+int gcd(int a, int b){
+    if(b == 0)
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
