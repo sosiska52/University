a4517982b66ac0f65a63102bed414dff8015528b
sosiska52 <nikuta.makarevich@gmail.com>
2024-05-20 13:32:03 +0300

    kmzi

--- 2 year/KMZI/src/kmzi_5/kmzi_5/kmzi_5.cpp
+++ 2 year/KMZI/src/kmzi_5/kmzi_5/kmzi_5.cpp
@@ -1,11 +1,87 @@
 ﻿#include <iostream>
 #include <cmath>
+#include <vector>
+#include <fstream>
 
-int gcd(int a, int b){
-    if(b == 0)
+void trialDivisions(int N) {
+    std::ifstream fin;
+    fin.open("prime_numbers.txt");
+    for (int i = 0; i < 300; i++) {
+        int primeNum;
+        fin >> primeNum;
+        if (N % primeNum == 0)
+            std::cout << primeNum << " ";
+    }
+    fin.close();
+}
+
+int gcd(int a, int b) {
+    while (b) {
+        int temp = b;
+        b = a % b;
+        a = temp;
+    }
+    return a;
+}
+
+int modular_pow(int base, int exponent, int mod) {
+    int result = 1;
+    base = base % mod;
+    while (exponent > 0) {
+        if (exponent % 2 == 1)
+            result = (result * base) % mod;
+        exponent = exponent >> 1;
+        base = (base * base) % mod;
+    }
+    return result;
+}
+
+int pollards_p_minus_1(int n, int B, int a) {
+    srand(time(0)); 
+    int g = gcd(a, n);
+    if (g > 1) {
+        return g;
+    }
+
+    for (int j = 2; j <= B; ++j) {
+        a = modular_pow(a, j, n);
+        g = gcd(a - 1, n);
+        if (g > 1 && g < n) {
+            return g;
+        }
+    }
+
+    return -1; 
+}
+
+bool findEl(std::vector<int> vec, int num) {
+    if (vec.empty())
+        return false;
+
+    for (int i = 0; i < vec.size(); i++) 
+        if (vec[i] == num)
+            return true;
+    return false;
 }
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    int number = 267584;
+    std::cout << "Trial divisions method: ";
+    trialDivisions(number);
+    std::cout << "\n";
+
+    std::vector<int>used;
+    for (int i = 1; i < number; i++) {
+        int result = pollards_p_minus_1(number, 100, i);
+        if (!findEl(used, result))
+            used.push_back(result);
+    }
+
+    std::cout << "Pollard: ";
+    for (int i = 1; i < used.size(); i++) {
+        std::cout << used[i] << " ";
+    }
+    std::cout << "\n";
+    return 0;
 }
