e0f6f23b94044c7e7b09068afb96bb06f5015fda
sosiska52 <nikuta.makarevich@gmail.com>
2024-09-07 13:10:23 +0300

    aipo_1

--- 3 year/AIPO/src/aipo_1/aipo_1/aipo_1.cpp
+++ 3 year/AIPO/src/aipo_1/aipo_1/aipo_1.cpp
@@ -1,8 +1,26 @@
-﻿#include <WinSock2.h>
-#include <Windows.h>
-#include <iostream>
+﻿#include <iostream>
+#include <WinSock2.h>
+#include <stdexcept>
+#include <system_error>
+
+class WinsockCL {
+private:
+	WSADATA wsaData{};
+public:
+	WinsockCL() {
+		const int startupResult = WSAStartup(WINSOCK_VERSION, &wsaData);
+		if (startupResult) {
+			throw std::system_error(startupResult, std::system_category());
+		}
+	}
+
+	~WinsockCL() {
+		const int cleanupResult = WSACleanup();
+	}
+};
 
 int main()
 {
+	WinsockCL winsock;
 	return 0;
 }
