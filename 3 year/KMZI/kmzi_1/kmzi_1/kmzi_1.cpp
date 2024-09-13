ac6fad948d772e20ee5a688c75986cc0d7c13475
Makarevich Nikita <129955309+sosiska52@users.noreply.github.com>
2024-09-13 10:22:48 +0300

    kmzi_1

---
+++ 3 year/KMZI/kmzi_1/kmzi_1/kmzi_1.cpp
@@ -0,0 +1,11 @@
+﻿#include <iostream>
+#include <openssl/rand.h>
+
+int main()
+{
+	unsigned char nonceA[16];
+	RAND_bytes(nonceA, sizeof(nonceA));
+	for (int i = 0; i < 16; i++) {
+		std::cout << int(nonceA[i]) << "\n";
+	}
+}
