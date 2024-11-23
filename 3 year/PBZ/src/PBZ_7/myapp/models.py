60fa6b6a349c7ec6353cc57602f69c2b5177701f
Makarevich Nikita <129955309+sosiska52@users.noreply.github.com>
2024-11-23 10:28:39 +0300

    pbz_7

--- 3 year/PBZ/src/PBZ_7/myapp/models.py
+++ 3 year/PBZ/src/PBZ_7/myapp/models.py
@@ -1,28 +1,27 @@
 from django.db import models
 
-class Товары(models.Model):
-    номер_товара = models.IntegerField(primary_key=True)
-    наименование = models.CharField(max_length=255)
-    единица_измерения = models.CharField(max_length=255)
-    цена = models.IntegerField()
+class Клиенты(models.Model):
+    номер_клиента = models.IntegerField(primary_key=True)
+    ФИО = models.CharField(max_length=255)
+    телефон = models.CharField(max_length=255)
+    адрес = models.CharField(max_length=255)
 
     def __str__(self):
-        return str(self.номер_товара)
+        return str(self.номер_клиента)
 
-class Чеки(models.Model):
-    номер_чека = models.IntegerField(primary_key=True)
-    номер_товара = models.IntegerField()
-    количество = models.IntegerField()
-    стоимость = models.IntegerField()
-
-    def __str__(self):
-        return str(self.номер_чека)
-
-class Продавцы(models.Model):
-    номер_чека = models.IntegerField(primary_key=True)
-    дата_покупки = models.DateField()
-    номер_кассы = models.IntegerField()
-    продавец = models.CharField(max_length=255)
+class Билеты(models.Model):
+    номер_клиента = models.IntegerField()
+    номер_фильма = models.IntegerField()
+    дата = models.DateField()
 
     def __str__(self):
-        return str(self.номер_чека)
+        return f"Клиент {self.номер_клиента}, Фильм {self.номер_фильма}"
+
+class Фильмы(models.Model):
+    номер_фильма = models.IntegerField(primary_key=True)
+    режиссер = models.CharField(max_length=255)
+    название = models.CharField(max_length=255)
+    год_выпуска = models.DateField()
+
+    def __str__(self):
+        return self.название
--- 3 year/PBZ/src/PBZ_7/myapp/views.py
+++ 3 year/PBZ/src/PBZ_7/myapp/views.py
@@ -1,3 +1,13 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from .models import Клиенты, Билеты, Фильмы
 
-# Create your views here.
+def home(request):
+    return render(request, 'cinema/home.html')
+
+def клиенты_list(request):
+    клиенты = Клиенты.objects.all()
+    return render(request, 'cinema/клиенты_list.html', {'клиенты': клиенты})
+
+def фильмы_list(request):
+    фильмы = Фильмы.objects.all()
+    return render(request, 'cinema/фильмы_list.html', {'фильмы': фильмы})
