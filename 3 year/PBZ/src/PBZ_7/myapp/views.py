4d153461bfc975ece7acf72b96a8ff6a4e238366
Makarevich Nikita <129955309+sosiska52@users.noreply.github.com>
2024-12-20 14:41:54 +0300

    pbz for Vlad

--- 3 year/PBZ/src/PBZ_7/myapp/views.py
+++ 3 year/PBZ/src/PBZ_7/myapp/views.py
@@ -1,13 +1,17 @@
 from django.shortcuts import render, get_object_or_404
-from .models import Клиенты, Билеты, Фильмы
+from .models import Заведующие, Категория, Группа
 
 def home(request):
     return render(request, 'cinema/home.html')
 
-def клиенты_list(request):
-    клиенты = Клиенты.objects.all()
-    return render(request, 'cinema/клиенты_list.html', {'клиенты': клиенты})
+def заведующие_list(request):
+    заведующие = Заведующие.objects.all()
+    return render(request, 'cinema/заведующие_list.html', {'заведующие': заведующие})
 
-def фильмы_list(request):
-    фильмы = Фильмы.objects.all()
-    return render(request, 'cinema/фильмы_list.html', {'фильмы': фильмы})
+def категории_list(request):
+    категории = Категория.objects.all()
+    return render(request, 'cinema/категории_list.html', {'категории': категории})
+
+def группы_list(request):
+    группы = Группа.objects.all()
+    return render(request, 'cinema/группы_list.html', {'группы': группы})
