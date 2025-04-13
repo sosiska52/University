be1ca84f03b8c8cfdd93dabb33521c3e4e1d2046
Makarevich Nikita <129955309+sosiska52@users.noreply.github.com>
2025-04-14 02:26:42 +0300

    ssvt_3

---
+++ 3 year/SSVT/src/ssvt_3/ssvt_2_2/ssvt_2_2.cpp
@@ -0,0 +1,87 @@
+﻿#include <windows.h>
+#include <ddraw.h>
+#include <tchar.h>
+
+#pragma comment(lib, "ddraw.lib")
+#pragma comment(lib, "dxguid.lib")
+#pragma comment(lib, "msimg32.lib")  // Добавлено для TransparentBlt
+
+LPDIRECTDRAW7 dd = nullptr;
+LPDIRECTDRAWSURFACE7 primarySurface = nullptr;
+HBITMAP hBitmapBackground = nullptr;
+HBITMAP hBitmapOverlay = nullptr;
+
+HBITMAP LoadBitmapFromFile(LPCWSTR filename) {
+    return (HBITMAP)LoadImageW(NULL, filename, IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+}
+
+LRESULT CALLBACK WindowProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam) {
+    switch (message) {
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        return 0;
+    case WM_PAINT: {
+        PAINTSTRUCT ps;
+        HDC hdc = BeginPaint(hWnd, &ps);
+        HDC hdcMem = CreateCompatibleDC(hdc);
+
+        if (hBitmapBackground) {
+            SelectObject(hdcMem, hBitmapBackground);
+            BitBlt(hdc, 0, 0, 800, 600, hdcMem, 0, 0, SRCCOPY);
+        }
+
+        if (hBitmapOverlay) {
+            SelectObject(hdcMem, hBitmapOverlay);
+            TransparentBlt(hdc, 0, 0, 800, 600, hdcMem, 0, 0, 800, 600, RGB(255, 255, 255));
+        }
+
+        DeleteDC(hdcMem);
+        EndPaint(hWnd, &ps);
+        return 0;
+    }
+    }
+    return DefWindowProc(hWnd, message, wParam, lParam);
+}
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow) {
+    WNDCLASS wc = { 0 };
+    wc.lpfnWndProc = WindowProc;
+    wc.hInstance = hInstance;
+    wc.lpszClassName = L"DirectDrawWindow";
+    RegisterClass(&wc);
+
+    HWND hWnd = CreateWindowW(L"DirectDrawWindow", L"DirectDraw в окне",
+        WS_OVERLAPPEDWINDOW, CW_USEDEFAULT, CW_USEDEFAULT,
+        800, 600, NULL, NULL, hInstance, NULL);
+
+    ShowWindow(hWnd, nCmdShow);
+    UpdateWindow(hWnd);
+
+    if (FAILED(DirectDrawCreateEx(NULL, (void**)&dd, IID_IDirectDraw7, NULL))) {
+        MessageBoxW(NULL, L"Ошибка DirectDraw!", L"Ошибка", MB_ICONERROR);
+        return 0;
+    }
+
+    if (FAILED(dd->SetCooperativeLevel(hWnd, DDSCL_NORMAL))) {
+        MessageBoxW(NULL, L"Ошибка SetCooperativeLevel!", L"Ошибка", MB_ICONERROR);
+        return 0;
+    }
+
+    hBitmapBackground = LoadBitmapFromFile(L"1.bmp");
+    hBitmapOverlay = LoadBitmapFromFile(L"2.bmp");
+
+    if (!hBitmapBackground || !hBitmapOverlay) {
+        MessageBoxW(NULL, L"Ошибка загрузки изображений!", L"Ошибка", MB_ICONERROR);
+        return 0;
+    }
+
+    InvalidateRect(hWnd, NULL, FALSE);
+
+    MSG msg = {};
+    while (GetMessage(&msg, NULL, 0, 0)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    return 0;
+}
